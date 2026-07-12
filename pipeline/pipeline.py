00746e745b89437777c206aa21ee369756b544b9
nensiG21 <nensi.ds.69@gmail.com>
2026-07-12 09:32:44 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+
+print("arguments", sys.argv)
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"day": [1, 2], "num_passanger": [3, 4]})
+df["month"] = month
+
+# day = int(sys.argv[2])
+print(df.head()) 
+
+df.to_parquet(f"output_{month}.parquet")
+print(f"Running pipeline for month {month}")
